a0bd8aaf00dd1ebf5f92349179686fb94adc3617
RaeGtHb <dian.andrae@gmail.com>
2026-03-01 19:41:26 +0800

    clickable links

--- README.md
+++ README.md
@@ -8,3 +8,25 @@
 - Grid & Flexbox
 -- Grid for template
 -- Flexbox for content
+
+- Logo
+-- create creative logo
+
+- Icons
+-- Connect my accounts
+
+- Header Container
+-- hover with same background color as projects container
+
+- Projects Container
+-- List?
+
+- Resume Container
+-- white background
+
+- About Me Container
+-- talk about myself, routine, hobby, etc.
+
+- Background Color
+-- #F5F5F5
+-- #EAEAEA
--- index.js
+++ index.js
@@ -1,12 +1,35 @@
+// Icons
 lucide.createIcons();
 
-setTimeout(() => {
-    const msg = document.getElementById("message");
-    msg.classList.remove("thinking");
-    msg.innerHTML = "Hi! There 👋";
-  }, 3000);
+// Message
+const message = document.getElementById("message");
+const chatBox = document.getElementById("chatBox");
+let timeout;
 
-  function showMessage() {
-    document.getElementById("text").classList.toggle("show");
-  }
-  
+function startAnimation() {
+  message.className = "chat-message thinking";
+  message.innerHTML = `
+    <div class="dots">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  `;
+
+  clearTimeout(timeout);
+
+  timeout = setTimeout(() => {
+    message.className = "chat-message";
+    message.innerHTML = "Hi! There 👋";
+  }, 3000);
+}
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      startAnimation();
+    }
+  });
+}, { threshold: 0.6 });
+
+observer.observe(chatBox);
